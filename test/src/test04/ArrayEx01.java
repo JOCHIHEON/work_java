299457990b6416792a436aafe3b10be3f27d89ae
KOITT <KOITT@KOITT-PC>
2018-07-28 18:21:35 +0900

    ddd

---
+++ test/src/test04/ArrayEx01.java
@@ -0,0 +1,10 @@
+package test04;
+
+public class ArrayEx01 {
+	public static void main(String[] args) {
+		int[] num = new int[5];
+		for (int i = 0; i < num.length; i++) {
+			num[i] += ;
+		}
+	}
+}
